46508981af3a07ee25935d240c354d7aa7e59934
Chris Anderson <jchris@gmail.com>
2023-09-18 13:26:39 -0700

    install fireproof

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import { useState } from 'react'
 import { FileDrop } from 'react-file-drop'
-
+import { fireproof } from 'use-fireproof'
 import './App.css'
 
 function App() {
   const theStyle = { background: '#bbb', width: '200px', height: '200px' }
 
-  const gotFiles = (files: FileList) => {
+  const gotFiles = (files: FileList| null) => {
     console.log('files:', files)
   }
 
   return (
     <>
       <div>
-
         <h1>Gallery</h1>
-
         <FileDrop onDrop={gotFiles}>
-          <div style={theStyle}>Drop some files here!</div>
+          <div style={theStyle}>Drop images here!</div>
         </FileDrop>
       </div>
     </>
