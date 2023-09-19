d06c1a9baf8aa96e968270ee355dceba8447d081
Chris Anderson <jchris@gmail.com>
2023-09-18 17:52:19 -0700

    basic grid

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,8 +1,34 @@
+import { useParams } from 'react-router-dom'
+import { useDocument, useFireproof } from 'use-fireproof'
 export function Upload() {
+  const { id } = useParams()
+  const { database, useLiveQuery } = useFireproof('gallery')
+
+  // const [uploadInfo] = useDocument(id)
+
+  const { docs } = useLiveQuery(
+    (doc, emit) => {
+      if (doc.type === 'file' && /image/.test(doc.contentType.toString())) {
+        emit(doc.uploadId)
+      }
+    },
+    { key: id }
+  )
+
   return (
     <>
-      <h1>Upload</h1>
-      <p>Upload page content</p>  
+      <h1 className='pb-4'>Upload {id}</h1>
+      <ul className="flex flex-wrap space-x-1 space-y-1">
+        {docs?.map(upload => (
+          <li key={upload._id} className="p-1 m-1">
+            <img
+              className="w-24 h-24"
+              src={`https://${upload.cid}.ipfs.w3s.link`}
+              alt={upload.name?.toString()}
+            />
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
